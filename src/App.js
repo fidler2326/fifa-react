97ae6875350343fac143f68586055615cc92eb1c
Adam <fidler2326@gmail.com>
2016-12-05 20:18:00 +0000

    Update to work with heroku

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import $ from 'jquery';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 var Team = React.createClass({
   render: function() {
     return (
